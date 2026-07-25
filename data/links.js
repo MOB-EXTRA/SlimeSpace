5085e8909c64760fadd08b9ac53deaa4bca10728
MOB-EXTRA <marcjohnfordc@gmail.com>
2026-07-25 23:05:32 +0800

    Deploying final tested updates

--- data/links.js
+++ data/links.js
@@ -3,7 +3,7 @@ const testServerData = {
     // 1 = Open | 0 = Closed
     status: 0,
     
-    lastUpdated: "July 12, 2026",
+    lastUpdated: "July 25, 2026",
     
     // Server Build Information
     updateDescription: "New Season Battle Pass, Collaboration Themes & Weapons", 
